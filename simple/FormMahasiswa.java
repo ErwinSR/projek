7ef94e50756d55f4e156e57b23f0afd1fdfc66e7
Erwin Sudrajat <44108496+ErwinSR@users.noreply.github.com>
2020-07-22 09:03:50 +0700

    delete white space

--- simple/FormMahasiswa.java
+++ simple/FormMahasiswa.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException; 
 import java.sql.PreparedStatement;
-import java .sql.ResultSet;
+import java.sql.ResultSet;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
