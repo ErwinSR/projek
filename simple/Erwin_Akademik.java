be171764c6a52b49897e4ea6647d9c2a74bb82ce
Erwin Sudrajat <44108496+ErwinSR@users.noreply.github.com>
2020-07-22 09:01:00 +0700

    Erwin_Akademik

---
+++ simple/Erwin_Akademik.java
@@ -0,0 +1,21 @@
+//************************************************
+//
+// Nama file: Akademik.java
+// Author: @Erwin Sudrajat R
+// Deskripsi: Latihan crud java
+// Tanggal: 02 - 07 - 2020
+//
+//************************************************
+
+package erwin_akademik;
+
+public class Erwin_Akademik {
+    
+    public static void main(String[] args) {
+        
+        FormMahasiswa mahasiswa = new FormMahasiswa();
+        mahasiswa.setVisible(true);
+        
+    }
+    
+}
